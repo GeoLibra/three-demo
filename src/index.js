143f4d6ea9d44dd863bc34af64d450cd88ab2234
GeoLibra <674530915@qq.com>
2021-12-15 19:55:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Map from './map.js';
+import { Map } from './map.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
